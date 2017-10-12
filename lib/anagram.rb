cf13954bb769ac0452181a2dbf299825118bb29b
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-12 02:23:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,35 +1,19 @@
-# Your code goes here!
 require 'pry'
 
 # Your code goes here!
+
 class Anagram
-attr_accessor :word
+  attr_accessor :word
 
   def initialize(word)
     @word = word
   end
 
-  #def match(array)
-  #  anagram = []
-  #  anagram_array = []
-  #  array.each do |word|
-  #    anagram_array = word.split("").select do |letter|
-  #      @word.include?(letter)
-  #    end
-  #  end
-  #  anagram << anagram_array.join
-  #end
-
   def match(array)
-    binding.pry
     anagram = []
-    tenacity = nil
     array.each do |word|
-      tenacity = word.split("").all? do |letter|
-        @word.include?(letter)
-        if tenacity == true
-          anagram << @word
-        end
+      if word.split("").sort == self.word.split("").sort
+        anagram << word
       end
     end
     anagram
